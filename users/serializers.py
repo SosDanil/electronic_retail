5334fdda72ed4ab33eeb0979665655578b9d4bb4
danil klimenko <sos.danil@yandex.ru>
2024-09-04 00:15:15 +0500

    добавил хеширование пароля на создание и обновление

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from users.models import User
@@ -8,3 +9,11 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super(UserSerializer, self).update(instance, validated_data)
--- users/views.py
+++ users/views.py
@@ -7,8 +7,3 @@ from users.serializers import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def perform_create(self, serializer):
-        user = serializer.save(is_active=True)
-        user.set_password(self.request.data('password'))
-        user.save()
