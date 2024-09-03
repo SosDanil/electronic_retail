f055a3faa5382c53a43bc34b60109b34945ffa9d
danil klimenko <sos.danil@yandex.ru>
2024-09-03 23:41:11 +0500

    создал вьюсет для пользователей

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(self.request.data('password'))
+        user.save()
