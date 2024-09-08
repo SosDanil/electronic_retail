7a5283915fe25efadef9ac25282f0ed69769f84d
danil klimenko <sos.danil@yandex.ru>
2024-09-08 14:45:16 +0500

    create CRUD for Company model

--- companies/urls.py
+++ companies/urls.py
@@ -1,7 +1,15 @@
+from django.urls import path
+
 from companies.apps import CompaniesConfig
+from companies.views import CompanyCreateAPIView, CompanyListAPIView, CompanyRetrieveAPIView, CompanyUpdateAPIView, \
+    CompanyDestroyAPIView
 
 app_name = CompaniesConfig.name
 
 urlpatterns = [
-
+    path('create/', CompanyCreateAPIView.as_view(), name='create_company'),
+    path('', CompanyListAPIView.as_view(), name='list_company'),
+    path('retrieve/<int:pk>/', CompanyRetrieveAPIView.as_view(), name='retrieve_company'),
+    path('update/<int:pk>/', CompanyUpdateAPIView.as_view(), name='update_company'),
+    path('delete/<int:pk>/', CompanyDestroyAPIView.as_view(), name='delete_company'),
 ]
--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+from companies.models import Company
+from companies.serializers import CompanySerializer, CompanyUpdateSerializer
+
+
+class CompanyCreateAPIView(CreateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class CompanyListAPIView(ListAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class CompanyRetrieveAPIView(RetrieveAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+
+class CompanyUpdateAPIView(UpdateAPIView):
+    queryset = Company.objects.all()
+    serializer_class = CompanyUpdateSerializer
+
+
+class CompanyDestroyAPIView(DestroyAPIView):
+    queryset = Company.objects.all()
